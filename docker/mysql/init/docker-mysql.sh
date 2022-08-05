a3fd7a542e44619a1bcd13951d45d87ecb6da053
azzamazizi <azzamazizi09@gmail.com>
2022-08-05 16:44:00 +0700

    update edit db import boxbilling

--- docker/mysql/init/docker-mysql.sh
+++ docker/mysql/init/docker-mysql.sh
@@ -2,4 +2,4 @@
 #docker exec bb-db /usr/bin/mysqldump -u root --password=azzam boxbilling > 02.sql
 
 # Restore
-cat 01.sql | docker exec -i bb-db /usr/bin/mysql -u root --password=azzam boxbilling_import
+cat 01.sql | docker exec -i bb-db /usr/bin/mysql -u root --password=azzam boxbilling
